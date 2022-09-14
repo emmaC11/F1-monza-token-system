94bc18bc416af50779380ea03c8f64aff9f251dd
EmmaC <c.emma2112@gmail.com>
2022-09-14 19:27:54 +0000

    create a user class

---
+++ user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, acc_num, f_name, l_name, seat_num, overdraft, balance):
+        self.acc_num = acc_num
+        self.f_name = f_name
+        self.l_name = l_name
+        self.seat_num = seat_num
+        self.overdraft = overdraft
+        self.token_bal = balance
