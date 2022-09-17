1fc517366031ac51fb55eff3fb46ccbef7d30afc
EmmaC <c.emma2112@gmail.com>
2022-09-17 16:42:22 +0000

    add display menu function that prints menu options

--- main-menu.py
+++ main-menu.py
@@ -1,3 +1,17 @@
 from user import User
 from file_parser import FileParser
 
+
+class menu():
+
+    def displayMenu(self, ds):
+        print("Welcome!")
+        print("Menu Options:")
+        print("1. View Current Users")
+        print("2. Add User")
+        print("3. Deposit Tokens")
+        print("4. Withdraw Tokens")
+        print("5. Transfer Tokens")
+        print("6. Search for User")
+        print("7. Help")
+        print("8. Exit Application")
