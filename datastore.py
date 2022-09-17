7c4cb6170308d58a3c0de7968e853146d91bfdf5
EmmaC <c.emma2112@gmail.com>
2022-09-17 16:11:27 +0000

    add a add user methods that appends the users list with the new user object

--- datastore.py
+++ datastore.py
@@ -11,4 +11,6 @@ class Datastore:
     def read_users(self):
         file_parser = FileParser()
         self._users = file_parser.read_users("users.txt")
-        
+
+    def add_user(self, user):
+        self._users.append(user)
