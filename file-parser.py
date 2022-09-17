f59b4b2ef4b3fe314827614316fbe3a7da8bafb3
EmmaC <c.emma2112@gmail.com>
2022-09-17 12:24:09 +0000

    add try block that uses the open function to read the file

--- file-parser.py
+++ file-parser.py
@@ -1,7 +1,18 @@
 from user import User
 
-class File_Parser:
-    
+class File_Parser():
+
     def read_users(self, filename):
         users = []
+
+        try:
+            file = open(filename, "read")
+
+            #file content stored in a list of strings
+            lines = file.readlines()
+            file.close()
+
+       
+
+
 
