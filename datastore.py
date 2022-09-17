0fddf2816088898ac5e77baba6b96283f7e7a6df
EmmaC <c.emma2112@gmail.com>
2022-09-17 16:16:24 +0000

    add docstrings to functions

--- datastore.py
+++ datastore.py
@@ -2,14 +2,17 @@ from file_parser import FileParser
 
 
 class Datastore:
+    """ class that calls the file parser to read users & can add users """
 
     def __init__(self):
         self._users = []
         self.read_users()
 
     def read_users(self):
+        """ creates instance of file parser object and reads users.txt file """
         file_parser = FileParser()
         self._users = file_parser.read_users("users.txt")
 
     def add_user(self, user):
+        """appends user list with a new user"""
         self._users.append(user)
