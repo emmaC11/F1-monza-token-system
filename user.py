1bd6ee4f6d36b864eea6454cda4bf54ad30fa7e4
EmmaC <c.emma2112@gmail.com>
2022-09-26 10:21:20 +0000

    remove redundant testing code from user.py

--- user.py
+++ user.py
@@ -25,71 +25,53 @@ class User:
     @property
     def acc_num(self):
         """account number getter"""
-        print("getting value")
         return self._acc_num
 
     @acc_num.setter
     def acc_num(self, value):
-        print("setting value")
         self._acc_num = value
 
     @property
     def f_name(self):
         """first name getter"""
-        print("getting value")
         return self._f_name
 
     @f_name.setter
     def f_name(self, value):
-        print("setting value")
         self._f_name = value
 
     @property
     def l_name(self):
         """last name getter"""
-        print("getting value")
         return self._l_name
 
     @l_name.setter
     def l_name(self, value):
-        print("setting value")
         self._l_name = value
 
     @property
     def seat_num(self):
         """seat number getter"""
-        print("getting value")
         return self._seat_num
 
     @seat_num.setter
     def seat_num(self, value):
-        print("setting value")
         self._seat_num = value
 
     @property
     def overdraft(self):
         """overdraft getter"""
-        print("getting value")
         return self._overdraft
 
     @overdraft.setter
     def overdraft(self, value):
-        print("setting value")
         self._overdraft = value
 
     @property
     def balance(self):
         """balance getter"""
-        print("getting value")
         return self.token_bal
 
     @balance.setter
     def balance(self, value):
-        print("setting value")
         self.token_bal = value
-
-
-# testUser = User(5, "emma", "c", "5", "yes", 5)
-# print(testUser.acc_num)
-
-
