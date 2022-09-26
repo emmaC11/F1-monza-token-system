78e472c427ab0effcfc4de4e9c7deccd3e7d1b66
EmmaC <c.emma2112@gmail.com>
2022-09-26 22:11:16 +0000

    add docstring to run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""file that runs the program, calls datastore & menu module"""
 from datastore import Datastore
 from menu import Menu
 
